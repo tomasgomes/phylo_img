b669201096e7238628a4fcb4b7f86d0d9670f49a
Tomas PiresdeCarvalhoGomes <tomas.pc.gomes@gmail.com>
2023-02-28 16:58:55 +0100

    Save images and taxa in one matrix

--- get_images.R
+++ get_images.R
@@ -3,6 +3,8 @@ library(jsonlite)
 
 library(taxize)
 
+library(Matrix)
+
 
 ekey = "24245f677350f2e5b323a147e20bca253708"
 Sys.setenv(ENTREZ_KEY=ekey)
@@ -18,7 +20,9 @@ links = "https://api.phylopic.org"
 # WARNING: this takes very long,
 ## will occasionally ask for used prompts, and may need to be restarted several times
 
-for(i in seq(0, npag-1)){
+ii = 0
+
+for(i in seq(ii, npag-1)){
   cat(i, sep = " ")
   imgpag = GET(paste0(linkp, i))
 
@@ -60,9 +64,26 @@ for(i in seq(0, npag-1)){
       write.csv(taxa, file = taxa_file, quote = F, row.names = F)
     }
   }
+  ii = i
 }
 
+# make unique taxa table
+tabs = list.files("data/csvs/", full.names = T)
+tab = lapply(tabs, function(f) read.csv(f, header = T))
+all_tab = reshape2::melt(tab)[,-c(10,11)]
+all_tab$hasTaxa = apply(all_tab[,2:8], 1, function(x) any(!is.na(x)))
+write.csv(all_tab, file = "data/all_taxa.csv", quote = F, row.names = F)
 
+# save images as a raw data matrix
+res_mat = matrix(0, length(all_tab$img_name), 128*128)
+rownames(res_mat) = all_tab$img_name
+for(img in all_tab$img_name){
+  imgmat = readPNG(paste0("data/imgs/", img, ".png"))[,,2]
+  res_mat[img,] = as.vector(imgmat)
+}
 
+#grid::grid.raster(matrix(res_mat[234,], 128, 128))
 
+res_mat = as(res_mat, "sparseMatrix")
+Matrix::writeMM(res_mat, "data/all_img_flat.mtx")
 
